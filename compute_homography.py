b65ca32734a674b9cc16f259a88c3fccca33fa36
Sébastien Gilbert <segilber@ca.ibm.com>
2021-06-08 10:37:30 -0400

    images/: 1st commit; compute_homography.py: Changed coordinates for the checkerboard image

--- compute_homography.py
+++ compute_homography.py
@@ -7,15 +7,15 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s \t
 
 def main():
     logging.info("compute_homography.py main()")
-    image_filepath = "./images/white_square.png"
-    corners_cm_to_pixels_dict =  {(4, 2): (1626, 2310, ),
-                                  (4, 11): (2101, 973),
-                                  (11, 11): (3134, 1274),
-                                  (11, 4): (2896, 2326)}
-    A = np.zeros((2 * len(corners_cm_to_pixels_dict), 6), dtype=float)
-    b = np.zeros((2 * len(corners_cm_to_pixels_dict), 1), dtype=float)
+    image_filepath = "./images/checkerboard.png"
+    features_cm_to_pixels_dict =  {(0, 0): (192, 801, ),
+                                  (214.2, 0): (937, 652),
+                                  (214.2, 178.5): (798, 67),
+                                  (0, 178.5): (101, 197)}
+    A = np.zeros((2 * len(features_cm_to_pixels_dict), 6), dtype=float)
+    b = np.zeros((2 * len(features_cm_to_pixels_dict), 1), dtype=float)
     index = 0
-    for XY, xy in corners_cm_to_pixels_dict.items():
+    for XY, xy in features_cm_to_pixels_dict.items():
         X = XY[0]
         Y = XY[1]
         x = xy[0]
@@ -31,22 +31,22 @@ def main():
         index += 1
     # A @ x = b
     x, residuals, rank, singular_values = np.linalg.lstsq(A, b, rcond=None)
-    #logging.debug("main(): x = {}".format(x))
-    #logging.debug("main(): x.shape = {}".format(x.shape))
+
     pixels_to_cm_transformation_mtx = np.array([[x[0, 0], x[1, 0], x[2, 0]], [x[3, 0], x[4, 0], x[5, 0]], [0, 0, 1]])
     logging.debug("main(): pixels_to_cm_transformation_mtx = \n{}".format(pixels_to_cm_transformation_mtx))
 
-    test_xy = (2888, 1650, 1)
-    test_XY = pixels_to_cm_transformation_mtx @ test_xy
-    logging.info("main(): test_xy = {}; test_XY = {}".format(test_xy, test_XY))
+    test_xy_1 = (399, 500, 1)
+    test_XY_1 = pixels_to_cm_transformation_mtx @ test_xy_1
+    logging.info("main(): test_xy_1 = {}; test_XY_1 = {}".format(test_xy_1, test_XY_1))
 
     cm_to_pixels_transformation_mtx = np.linalg.inv(pixels_to_cm_transformation_mtx)
-    XY_5_9 = (5, 9, 1)
-    xy_5_9 = cm_to_pixels_transformation_mtx @ XY_5_9
+    test_XY_2 = (107.1, 107.1, 1)
+    test_xy_2 = cm_to_pixels_transformation_mtx @ test_XY_2
     image = cv2.imread(image_filepath)
     annotated_img = copy.deepcopy(image)
-    draw_crosshair(annotated_img, xy_5_9, 20, (0, 255, 0))
-    
+    draw_crosshair(annotated_img, (round(test_xy_2[0]), round(test_xy_2[1])), 20, (0, 255, 0))
+    cv2.imwrite("./outputs/annotated.png", annotated_img)
+
 
 def draw_crosshair(image, center, width, color):
     cv2.line(image, (center[0] - width//2, center[1]), (center[0] + width//2, center[1]), color)
